dba2f26eff15ea42ea53660acca961d31bb35a2c
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-08-19 14:58:56 +0530

    WhatsAlexa: First Release ✅️

--- plugins/spdf.js
+++ plugins/spdf.js
@@ -1,22 +1,36 @@
-const Asena = require('../events');
-const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
-const fs = require('fs');
-const axios = require('axios');
-const request = require('request');
-const got = require("got");
-const Config = require('../config');
-const SPDF_DESC = "Converts a Site into PDF"
-const SPDF_PROC = "```Converting Site into PDF```"
-const SPDF_LINK = "*Must Enter a URL*"
+let WhatsAlexa = require('../events');
+let {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+let fs = require('fs');
+let axios = require('axios');
+let request = require('request');
+let got = require("got");
+let Config = require('../config');
 
-Asena.addCommand({pattern: 'spdf ?(.*)', fromMe: false, desc: SPDF_DESC }, (async (message, match) => {
+if (Config.WORKTYPE == 'private') {
 
-    if (match[1] === '') return await message.sendMessage(SPDF_LINK);
+    WhatsAlexa.addCommand({pattern: 'spdf ?(.*)', fromMe: true, desc: Lang.SPDF_DESC }, (async (message, match) => {
+
+    if (match[1] === '') return await message.sendMessage(Lang.SPDF_LINK);
 
     var webimage = await axios.get(`https://api.html2pdf.app/v1/generate?url=${match[1]}&apiKey=begC4dFAup1b8LyRXxAfjetfqDg2uYx8PWmh9YJ59tTZXiUyh2Vs72HdYQB68vyc`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(SPDF_PROC);
+    await message.sendMessage(Lang.SPDF_PROC);
 
-    await message.sendMessage(Buffer.from(webimage.data), MessageType.document, {mimetype: Mimetype.pdf})
+    await message.sendMessage(Buffer.from(webimage.data), MessageType.document, {mimetype: Mimetype.pdf, filename: 'WhatsAlexa-Site-To-PDF-Feature-Made-By-Toxic-Devil.pdf})
 
-}));   
+    }));  
+}
+else if (Config.WORKTYPE == 'public') {
+
+    WhatsAlexa.addCommand({pattern: 'spdf ?(.*)', fromMe: false, desc: Lang.SPDF_DESC }, (async (message, match) => {
+
+    if (match[1] === '') return await message.sendMessage(Lang.SPDF_LINK);
+
+    var webimage = await axios.get(`https://api.html2pdf.app/v1/generate?url=${match[1]}&apiKey=begC4dFAup1b8LyRXxAfjetfqDg2uYx8PWmh9YJ59tTZXiUyh2Vs72HdYQB68vyc`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Lang.SPDF_PROC);
+
+    await message.sendMessage(Buffer.from(webimage.data), MessageType.document, {mimetype: Mimetype.pdf, filename: 'WhatsAlexa-Site-To-PDF-Feature-Made-By-Toxic-Devil.pdf})
+
+    }));   
+}
