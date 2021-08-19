e0b6ff7700a7694223728a7de7a5f98ebe0d9ec1
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-08-19 20:22:28 +0530

    Downgraded

--- bot.js
+++ bot.js
@@ -69,7 +69,7 @@ async function Alexa () {
     
     const conn = new WAConnection();
     const Session = new StringSession();
-    conn.version = [2, 2126, 14]
+    conn.version = [2, 2119, 6]
 
     conn.logger.level = config.DEBUG ? 'debug' : 'warn';
     var nodb;
